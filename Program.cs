353d0cdd7bd778b448cf9c71c6b78490b076c519
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2021-03-07 15:17:50 -0500

    Created test over console.write.

--- Program.cs
+++ Program.cs
@@ -6,26 +6,8 @@ namespace Wumpus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            int niveau = 1;
-            bool continuer = true;
-            int score_global = 0;
-
-            while(continuer){
-                Partie partie = new Partie(niveau);
-                score_global += partie.Jouer();
-
-                Console.WriteLine("le score actuel à la fin du niveau " + niveau + " est de " + score_global);
-                Console.Write("continuer ? (Y/N) : ");
-                
-                string r = Console.ReadLine();
-                if(r == "N" || r == "n"){
-                    continuer = false;
-                }
-                
-                niveau++;
-            }
+            Engine engine = new Engine();
+            engine.Start();
         }
     }
 }
---
+++ tests/EngineOutput.cs
@@ -0,0 +1,29 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+using Wumpus;
+
+namespace tests
+{
+    public class EngineOutput
+    {
+        StringWriter sw = new StringWriter();
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.SetOut(sw);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Engine engine = new Engine();
+            engine.Start();
+
+            string expected = string.Format("Hello World!{0}", Environment.NewLine);
+
+            Assert.AreEqual(expected, sw.ToString());
+        }
+    }
+}
